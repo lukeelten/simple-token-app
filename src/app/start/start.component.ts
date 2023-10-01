4a644d8016ef8fb048fbff36a0db27ae6c9c998d
Tobias Derksen <t.derksen@mailbox.org>
2023-10-01 13:13:38 +0200

    Add components

---
+++ src/app/start/start.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  template: `
+    <p>
+      start works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class StartComponent {
+
+}
---
+++ src/app/token/token.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-token',
+  template: `
+    <p>
+      token works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TokenComponent {
+
+}
