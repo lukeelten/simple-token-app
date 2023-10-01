62a4036d71fd2e354876ffc38594cc4af2e87039
Tobias Derksen <t.derksen@mailbox.org>
2023-10-01 13:13:27 +0200

    Update angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StartComponent} from "./start/start.component";
+import {TokenComponent} from "./token/token.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StartComponent
+  },
+  {
+    path: '/token',
+    component: TokenComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { TokenComponent } from './token/token.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+      keycloak.init({
+        config: {
+          url: 'http://localhost:8080',
+          realm: 'your-realm',
+          clientId: 'your-client-id'
+        },
+        initOptions: {
+          onLoad: 'check-sso',
+          silentCheckSsoRedirectUri:
+              window.location.origin + '/assets/silent-check-sso.html'
+        }
+      });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    TokenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
